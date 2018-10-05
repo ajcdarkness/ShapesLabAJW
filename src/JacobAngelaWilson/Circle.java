d8594ad682b3a713434affc922e7760c39959b02
azheng8072 <43213098+azheng8072@users.noreply.github.com>
2018-10-05 11:34:52 -0400

    comment

--- src/JacobAngelaWilson/Circle.java
+++ src/JacobAngelaWilson/Circle.java
@@ -3,14 +3,16 @@ package JacobAngelaWilson;
 public class Circle extends Shape{
     private int radius;
 
+    //specifies radius
     public Circle(int theRadius){
         super("Circle");
         radius = theRadius;
-    }//creates getters for area and perimeter
+    }//returns area
     public double getArea(){
         double area = Math.PI*radius*radius;
         return area;
     }
+    //returns perimeter
     public double getPerimeter(){
         double perimeter = 2*Math.PI * radius;
         return perimeter;
