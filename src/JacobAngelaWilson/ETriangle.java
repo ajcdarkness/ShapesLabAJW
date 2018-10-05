b0cf174f59975d9412a77b75c55c24b0393006e0
azheng8072 <43213098+azheng8072@users.noreply.github.com>
2018-10-05 11:33:51 -0400

    commented

--- src/JacobAngelaWilson/ETriangle.java
+++ src/JacobAngelaWilson/ETriangle.java
@@ -1,17 +1,20 @@
 package JacobAngelaWilson;
-//creates the equilateral triangle class
+
 public class ETriangle extends Shape
 {
+    //defines the sides
     private int side;
-
+    // specifies sides based on the values given
     public  ETriangle(int side){
         super("ETriangle");
         this.side = side;
-    }//creates the area and perimeter for Etriangles with given side lengths
+    }
+    //returns area based on given side lengths
     public double getArea()
     {
         return (Math.sqrt(3)/4) * side * side;
     }
+    //returns perimeter based on given sides
     public double getPerimeter() {
         return side * 3;
     }
